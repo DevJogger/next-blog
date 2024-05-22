e745db5040c7e1398f2fc5bc04b30410dbeacdb0
DevJogger <158173870+DevJogger@users.noreply.github.com>
2024-05-22 10:43:09 -0700

    Fix ESLint/prettier errors

--- components/post/CircleProgressIndicatorsArbitrary.tsx
+++ components/post/CircleProgressIndicatorsArbitrary.tsx
@@ -9,7 +9,7 @@ export default function CircleProgressIndicatorsArbitrary() {
         </div>
         <h2 className="m-0">Loading Progress</h2>
       </div>
-      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 sm:flex dark:bg-stone-600">
+      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 dark:bg-stone-600 sm:flex">
         <div className="relative grid h-40 w-40 place-items-center rounded-full bg-[conic-gradient(var(--tw-gradient-stops))] from-[#f5b700] from-0% via-[#f5b700] via-[63%] to-gray-700 to-[63%] before:absolute before:inset-3 before:rounded-full before:bg-stone-200 before:content-[''] before:dark:bg-stone-600">
           <h1 className="absolute m-0">
             63<small>%</small>
@@ -17,7 +17,7 @@ export default function CircleProgressIndicatorsArbitrary() {
         </div>
         <h2 className="m-0">Rendering Tracker</h2>
       </div>
-      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 md:flex dark:bg-stone-600">
+      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 dark:bg-stone-600 md:flex">
         <div className="relative grid h-40 w-40 place-items-center rounded-full bg-[conic-gradient(var(--tw-gradient-stops))] from-[#dc0073] from-0% via-[#dc0073] via-[84%] to-gray-700 to-[84%] before:absolute before:inset-3 before:rounded-full before:bg-stone-200 before:content-[''] before:dark:bg-stone-600">
           <h1 className="absolute m-0">
             84<small>%</small>
--- components/post/CircleProgressIndicatorsBasicTailwind.tsx
+++ components/post/CircleProgressIndicatorsBasicTailwind.tsx
@@ -9,7 +9,7 @@ export default function CircleProgressIndicatorsBasicTailwind() {
         </div>
         <h2 className="m-0">Loading Progress</h2>
       </div>
-      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 sm:flex dark:bg-stone-600">
+      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 dark:bg-stone-600 sm:flex">
         <div className="relative grid h-40 w-40 place-items-center rounded-full bg-[conic-gradient(var(--tw-gradient-stops))] from-amber-400 from-0% via-amber-400 via-65% to-gray-700 to-65% before:absolute before:inset-3 before:rounded-full before:bg-stone-200 before:content-[''] before:dark:bg-stone-600">
           <h1 className="absolute m-0">
             65<small>%</small>
@@ -17,7 +17,7 @@ export default function CircleProgressIndicatorsBasicTailwind() {
         </div>
         <h2 className="m-0">Rendering Tracker</h2>
       </div>
-      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 md:flex dark:bg-stone-600">
+      <div className="hidden flex-auto flex-col items-center gap-3 rounded-xl bg-stone-200 p-7 dark:bg-stone-600 md:flex">
         <div className="relative grid h-40 w-40 place-items-center rounded-full bg-[conic-gradient(var(--tw-gradient-stops))] from-pink-700 from-0% via-pink-700 via-85% to-gray-700 to-85% before:absolute before:inset-3 before:rounded-full before:bg-stone-200 before:content-[''] before:dark:bg-stone-600">
           <h1 className="absolute m-0">
             85<small>%</small>
